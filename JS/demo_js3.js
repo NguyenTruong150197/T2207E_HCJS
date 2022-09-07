3cbcf01b8771c61cb0c859f96c8c071e165f5cdc
NguyenTruong150197 <truong12121996@gmail.com>
2022-09-07 21:13:42 +0700

    update

--- JS/demo_js3.js
+++ JS/demo_js3.js
@@ -15,3 +15,17 @@ function changeText(){
     h.style.transform = "rotate("+ x + "deg)";
     x++;
 }
+
+function inputSeach(obj){
+    //var ix = document.getElementById(id);
+    console.log("Seaching..." + obj.value);
+}
+
+function demmoEvent2(){
+    //var ips = document.getElementsByTagName("input"); // array
+    //var ips = document.getElementsByClassName("input-text");
+    var ips = document.querySelectorAll("input.input-text[type=text]");
+    for(var i = 0; i < ips.length; i++){
+        ips[i].style.backgroundColor = "red";
+    }
+}

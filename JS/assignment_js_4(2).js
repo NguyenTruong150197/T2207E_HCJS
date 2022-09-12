e11eb631a15134a7e497f2f40a7de48e7cc2bdb6
NguyenTruong150197 <truong12121996@gmail.com>
2022-09-12 19:04:28 +0700

    update

--- JS/assignment_js_4(2).js
+++ JS/assignment_js_4(2).js
@@ -13,7 +13,7 @@ function forecastAjax(){
             document.getElementById("forecast").innerHTML += f;
         }
     }
-    var link = "http://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    var link = "https://api.openweathermap.org/data/2.5/forecast?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
     xhttp.open("get", link, true);
     xhttp.send();
 }
